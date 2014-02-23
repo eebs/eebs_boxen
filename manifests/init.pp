481ebc9f76e95c1bc6f001be39ce13db739b06b0
Eebs Kobeissi <eebs@eebs-tech.com>
2014-02-23 00:06:43 -0500

    added custom packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
 class eebs_boxen {
+	# Package includes, these will require
+	# the corresponding module to be installed
+	include chrome
+    include adium
+    include vlc
+
+    # Custom packages
+    package { "htop": }
+
+    # Custom classes
     include eebs_boxen::dotfiles
     include eebs_boxen::gitoverrides
 }
