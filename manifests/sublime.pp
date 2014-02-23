6604f66b95c4d7b6ef65a8acb021534df92b0998
Eebs Kobeissi <eebs@eebs-tech.com>
2014-02-23 00:14:05 -0500

    added sublime

---
+++ manifests/sublime.pp
@@ -0,0 +1,25 @@
+class eebs_boxen::sublime {
+
+    include sublime_text_3
+    include sublime_text_3::package_control
+
+    sublime_text_3::package { 'sublime-text-puppet':
+        source => 'eklein/sublime-text-puppet'
+    }
+
+    sublime_text_3::package { 'sublime-kurogo-debug':
+        source => 'eebs/sublime-kurogo-debug'
+    }
+
+    file { 'user-preferences':
+        path    => '/Users/eebs/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings',
+        ensure  => file,
+        source  => 'puppet:///modules/eebs_boxen/sublime/Preferences.sublime-settings'
+    }
+
+    file { 'user-keybindings':
+        path    => '/Users/eebs/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap',
+        ensure  => file,
+        source  => 'puppet:///modules/eebs_boxen/sublime/Default (OSX).sublime-keymap'
+    }
+}
