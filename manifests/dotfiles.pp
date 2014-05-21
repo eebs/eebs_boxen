9aa8e0686f003ebab43b019db216ab214374eecf
Eebs Kobeissi <eebs@eebs-tech.com>
2014-05-21 09:17:38 -0400

    updated dotfiles location

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -1,15 +1,20 @@
 class eebs_boxen::dotfiles {
 
     $home     = "/Users/${::boxen_user}"
-    $my       = "${home}/my"
-    $dotfiles = "${my}/dotfiles"
+    $code     = "${home}/code"
+    $eebs     = "${code}/eebs"
+    $dotfiles = "${eebs}/dotfiles"
 
-    file { $my:
-        ensure  => directory
+    file {
+        [
+            $code,
+            $eebs
+        ]:
+        ensure => directory,
     }
 
     repository { $dotfiles:
         source  => 'eebs/dotfiles',
-        require => File[$my],
+        require => File[$eebs],
     }
 }
